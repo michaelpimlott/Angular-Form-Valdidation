cb9825f0cff59d758508f68825ad13dd437fc1b5
Michael Pimlott <pettingbutterscotch@gmail.com>
2016-07-26 20:18:41 -0700

    add module and controller to app.js

---
+++ app.js
@@ -0,0 +1,9 @@
+var testApp = angulur.module('testApp', []);
+
+testApp.controller('mainController', function($scope){
+  $scope.submitForm = function(isValid) {
+    if(isValid) {
+      console.log('Your old grandpa is proud of you, kiddo.');
+    }
+  };
+});
